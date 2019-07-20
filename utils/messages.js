728f46942b926a03f6a7411e8c60e028ea5b3480
uhmtoto <uhmtotow@gmail.com>
2019-07-20 19:57:06 +0900

    feat: change error message

--- utils/messages.js
+++ utils/messages.js
@@ -6,7 +6,7 @@ export default {
   'placeNotExist': '존재하지 않는 장소입니다.',
   'checkPayload': '입력하신 페이로드를 확인해주세요.',
   'checkAccount': '아이디 또는 비밀번호를 확인해주세요.',
-  'failAuth': '인증 헤더에 문제가 있습니다.',
+  'failAuth': '인증을 실패했습니다.',
   'noPermission': '해당 작업을 수행하기 위한 권한이 없습니다.',
   'underDev': '구현되지 않은 엔드포인트입니다.',
   'success': '요청한 작업을 성공적으로 처리했습니다.',
