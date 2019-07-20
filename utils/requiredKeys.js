dca8df243281d6e8ebf45272e0f61e6da1c3f4c6
uhmtoto <uhmtotow@gmail.com>
2019-07-20 22:36:52 +0900

    style: remove unnecessary single quote

--- utils/requiredKeys.js
+++ utils/requiredKeys.js
@@ -1,7 +1,7 @@
 export default {
-  'createDevice': ['type', 'name', 'owners', 'desc', 'methods'],
-  'authorizeUser': ['id', 'password'],
-  'createUser': ['id', 'password', 'name'],
-  'createPlace': ['type', 'name', 'desc', 'geocode'],
-  'editPlace': ['type', 'name', 'desc', 'geocode', 'registrar']
+  createDevice: ['type', 'name', 'owners', 'desc', 'methods'],
+  authorizeUser: ['id', 'password'],
+  createUser: ['id', 'password', 'name'],
+  createPlace: ['type', 'name', 'desc', 'geocode'],
+  editPlace: ['type', 'name', 'desc', 'geocode', 'registrar']
 }
