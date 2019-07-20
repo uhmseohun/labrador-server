cd83eafe3b2fbe9ed752cd8f01e814be8063371b
uhmtoto <uhmtotow@gmail.com>
2019-07-20 18:42:17 +0900

    feat: place get, post, put routes

--- routes/place.js
+++ routes/place.js
@@ -2,6 +2,8 @@ import { Router } from 'express'
 
 import models from '../models'
 import messages from '../utils/messages'
+import verify from '../utils/verify'
+import requiredKeys from '../utils/requiredKeys'
 
 const router = Router()
 
@@ -9,14 +11,31 @@ const router = Router()
  * @summary 등록되어 있는 모든 장소들을 반환합니다.
  */
 router.get('/', (req, res, next) => {
-  next(models.Error(423, messages.underDev))
+  models.Place.find({})
+    .then(places => {
+      res.json(places)
+    })
+    .catch(error => {
+      res.json(models.Error(500, messages.dbError))
+    })
 })
 
 /**
  * @summary 새로운 장소를 등록합니다.
  */
 router.post('/', (req, res, next) => {
-  next(models.Error(423, messages.underDev))
+  if (!verify.keys(req, res, requiredKeys.createPlace)) {
+    return next(models.Error(400, messages.checkPayload))
+  }
+
+  let newPlace = new models.Place()
+  newPlace = Object.assign(newPlace, req.body)
+  newPlace.registrar = [req.user._id]
+
+  newPlace.save(error => {
+    if (error) next(error)
+    res.json({ message: messages.success })
+  })
 })
 
 /**
@@ -24,15 +43,44 @@ router.post('/', (req, res, next) => {
  * 장소의 관리자만 접근할 수 있습니다.
  */
 router.delete('/:placeId', (req, res, next) => {
-  next(models.Error(423, messages.underDev))
+  models.Place.findById(req.params.placeId)
+    .then(place => {
+      if (!place) return next(models.Error(404, messages.placeNotExist))
+      if (!place.registrar.includes(req.user._id)) {
+        return next(models.Error(403, messages.noPermission))
+      }
+
+      place.remove()
+      res.json({ message: messages.success })
+    })
+    .catch(error => {
+      return next(models.Error(500, messages.dbError))
+    })
 })
 
 /**
  * @summary 등록되어 있는 장소의 정보를 수정합니다.
  * 장소의 관리자만 접근할 수 있습니다.
  */
-router.put('/:deviceId', (req, res, next) => {
-  next(models.Error(423, messages.underDev))
+router.put('/:placeId', (req, res, next) => {
+  models.Place.findById(req.params.placeId)
+    .then(place => {
+      if (!place) return next(models.Error(404, message.placeNotExist))
+      if (!place.registrar.includes(req.user._id)) {
+        return next(models.Error(403, messages.noPermission))
+      }
+
+      requiredKeys.editPlace.forEach(v => {
+        place[v] = req.body[v]
+      })
+
+      place.save()
+      res.json({ message: messages.success })
+    })
+    .catch(error => {
+      console.error(error)
+      next(models.Error(500, messages.dbError))
+    })
 })
 
 export default router
--- utils/requiredKeys.js
+++ utils/requiredKeys.js
@@ -1,5 +1,7 @@
 export default {
   'createDevice': ['type', 'name', 'owners', 'desc', 'methods'],
   'authorizeUser': ['id', 'password'],
-  'createUser': ['id', 'password', 'name']
+  'createUser': ['id', 'password', 'name'],
+  'createPlace': ['type', 'name', 'desc', 'geocode'],
+  'editPlace': ['type', 'name', 'desc', 'geocode', 'registrar']
 }
