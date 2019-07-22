f9ecc1692a7d27ba8294fa24cb3d4aee7ccbe8c4
uhmtoto <uhmtotow@gmail.com>
2019-07-23 08:24:20 +0900

    fix: verify payload logic

--- utils/verify/keys.js
+++ utils/verify/keys.js
@@ -2,7 +2,7 @@
  * @summary 페이로드의 키를 검증합니다.
  */
 export default (req, res, keys) => {
-  let emptyKeys = keys.filter(v => !req.body.hasOwnProperty(v))
+  let emptyKeys = keys.filter(v => !req.body[v])
   return emptyKeys.length === 0 &&
     keys.length === Object.keys(req.body).length
 }
