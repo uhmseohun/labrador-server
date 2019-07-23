2d7c483c443e064d59b025f658ae0f7b64a18166
uhmtoto <uhmtotow@gmail.com>
2019-07-23 10:50:25 +0900

    feat: notification routes

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ import device from './device'
 import place from './place'
 import user from './user'
 import labrador from './labrador'
+import notification from './notification'
 
 export default {
   auth,
   device,
   place,
   user,
-  labrador
+  labrador,
+  notification
 }
